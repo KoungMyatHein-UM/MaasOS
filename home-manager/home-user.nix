0120f193bb766d3c6e8b7938c260a50870ccc010
KhakiKukhi <a@a.a>
2026-01-28 21:29:17 +0100

    Add `home-user.nix` with initial Home Manager configuration

---
+++ home-manager/home-user.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  home.stateVersion = "25.11";
+
+  home.packages = with pkgs; [
+  ];
+
+  programs.bash.enable = true;
+  programs.git = {
+    enable = true;
+  };
+
+  programs.firefox = {
+      enable = true;
+      profiles.maassec = {
+        id = 0;
+        isDefault = true;
+        settings = {
+          "browser.startup.homepage" = "https://github.com/KoungMyatHein-UM/MaasOS";
+          "browser.search.region" = "NL";
+          "browser.shell.checkDefaultBrowser" = true;
+        };
+
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          #ublock-origin
+          #bitwarden
+        ];
+      };
+    };
+}
