86725e0fd294cc598d9c42ec149e6e32883ada01
KhakiKukhi <a@a.a>
2026-01-28 21:47:56 +0100

    Enable system bootloader, set kernel packages, and update user description in `user.nix`.

--- system/boot.nix
+++ system/boot.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 
 {
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "nvme" "sd_mod" ];
 
-  #boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages;
 }
--- user.nix
+++ user.nix
@@ -3,7 +3,7 @@
 {
   users.users.MaasSec = {
     isNormalUser = true;
-    description = "MaasSec Organizational User";
+    description = "MaasSec";
     extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
     initialPassword = "MaasSec";
     shell = pkgs.bash;
