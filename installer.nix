0d1b606aac49f396fef84305c321b4a56db60cb8
KhakiKukhi <a@a.a>
2026-01-28 21:50:35 +0100

    Enable Plasma 6 graphical installer and welcome widget in `installer.nix`

--- installer.nix
+++ installer.nix
@@ -2,9 +2,12 @@
 
 {
   imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+#    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-plasma6.nix"
     "${modulesPath}/installer/cd-dvd/channel.nix"
   ];
 
   installer.cloneConfig = true;
+
+  welcomeWidget.enable = true;
 }
