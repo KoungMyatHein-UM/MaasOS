c52eaeb37ade626c466a9fc4d51ae1944ae9a87a
KhakiKukhi <a@a.a>
2026-01-28 22:57:08 +0100

    Refactor wallpaper setup: use `${wallpaper_file}` variable in autostart entry and improve SDDM configuration rules

--- system/desktop.nix
+++ system/desktop.nix
@@ -5,21 +5,15 @@ let
 in
 {
   services.desktopManager.plasma6.enable = true;
-  services.displayManager = {
-      sddm = {
-        enable = true;
-        wayland.enable = true;
-
-        settings = {
-          Theme = {
-            CursorTheme = "breeze_cursors";
-          };
-        };
-      };
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
     };
 
     systemd.tmpfiles.rules = [
-        "L+ /usr/share/sddm/themes/breeze/MaasSec_BG.png - - - - ${wallpaper}"
+        "L+ /usr/share/sddm/themes/breeze/MaasSec_BG.png - - - - ${wallpaper}" [cite: 29]
+        "d /var/lib/sddm/.config 0700 sddm sddm - -"
+        "f /usr/share/sddm/themes/breeze/theme.conf.user 0644 root root - Background=MaasSec_BG.png"
       ];
 }
 
