ff70082149ba21c148059cbe71a69846c4252c39
KhakiKukhi <a@a.a>
2026-01-29 00:58:24 +0100

    Refactor wallpaper setup: improve SDDM theme configuration and optimize autostart wallpaper application logic

--- home-manager/home-user.nix
+++ home-manager/home-user.nix
@@ -1,17 +1,18 @@
 { pkgs, lib, ... }:
 
 let
-  wallpaper_file = ./maassec_wallpaper.png;
+  wallpaper = ./maassec_wallpaper.png;
 in
 {
   home.stateVersion = "25.11";
 
-  home.file.".local/share/wallpapers/maassec_wallpaper.png".source = wallpaper_file;
+  home.file.".local/share/wallpapers/maassec_wallpaper.png".source = wallpaper;
   home.file.".config/autostart/set-wallpaper.desktop".text = ''
       [Desktop Entry]
       Type=Application
       Name=Set MaasOS Wallpaper
-      Exec=sh -c "sleep 15 && ${pkgs.kdePackages.plasma-desktop}/bin/plasma-apply-wallpaperimage ${wallpaper_file}"
+      # The script waits up to 20 seconds for plasmashell to appear
+      Exec=sh -c "for i in {1..20}; do if pgrep -x plasmashell > /dev/null; then sleep 2; ${pkgs.kdePackages.plasma-desktop}/bin/plasma-apply-wallpaperimage ${wallpaper}; break; fi; sleep 1; done"
       X-KDE-Autostart-phase=2
     '';
 
--- system/desktop.nix
+++ system/desktop.nix
@@ -5,21 +5,25 @@ let
 in
 {
   services.desktopManager.plasma6.enable = true;
+
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-
-    settings = {
-      Theme = {
-        CursorTheme = "breeze_cursors";
-      };
-    };
+    theme = "maassec-breeze";
   };
 
-  environment.etc."sddm/themes/breeze/theme.conf.user".text = ''
-    [General]
-    background=${wallpaper}
-  '';
+  environment.systemPackages = [
+    (pkgs.runCommand "maassec-sddm-theme" { } ''
+      mkdir -p $out/share/sddm/themes/maassec-breeze
+      # Copy the original Breeze theme structure
+      cp -r ${pkgs.kdePackages.sddm-kcm}/share/sddm/themes/breeze/* $out/share/sddm/themes/maassec-breeze/
+
+      # Overwrite the background setting in the theme config
+      chmod +w $out/share/sddm/themes/maassec-breeze/theme.conf
+      echo "[General]" > $out/share/sddm/themes/maassec-breeze/theme.conf.user
+      echo "background=${wallpaper}" >> $out/share/sddm/themes/maassec-breeze/theme.conf.user
+    '')
+  ];
 }
 
 
