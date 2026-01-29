6adfe07caec81bd8e65324e61db264f8eaa2782a
KhakiKukhi <a@a.a>
2026-01-29 01:41:23 +0100

    Swap to enable Plasma 6 graphical installer and disable minimal installer in `installer.nix`

--- installer.nix
+++ installer.nix
@@ -2,8 +2,8 @@
 
 {
   imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
-#    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
+#    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
     "${modulesPath}/installer/cd-dvd/channel.nix"
   ];
 
