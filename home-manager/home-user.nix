eb75b8121ce8106b20a10bf34095d3271d4080b1
KhakiKukhi <a@a.a>
2026-01-29 02:30:04 +0100

    Refactor: extract `home.packages` to `user-packages.nix` and update imports in `home-user.nix`

--- home-manager/home-user.nix
+++ home-manager/home-user.nix
@@ -6,6 +6,8 @@
 #    '';
 #in
 {
+  imports = [ ./user-packages.nix ];
+
   nixpkgs.config.allowUnfree = true;
 
   home.stateVersion = "25.11";
@@ -24,74 +26,6 @@
 #    ];
 #
 #    home.file.".local/share/wallpapers/maassec_wallpaper.png".source = ./maassec_wallpaper.png;
-
-  home.packages = with pkgs; [
-    ### network analysis
-    # wireshark - in system packages: requires low-level system access
-    # tcpdump - same as wireshark
-    sniffnet
-    aircrack-ng
-    ethtool
-    util-linux
-    net-tools
-
-    ### web security
-    burpsuite
-    zap # aka zaproxy
-    nikto
-    sqlmap
-    dirb
-    # curl - already exists by default
-    wget
-
-    ### vulnerability assessment
-    openvas-scanner
-    thc-hydra
-    john
-    hashcat
-
-    ### reverse engineering
-    binwalk
-    binutils
-    strace
-    ltrace
-    steghide
-    wineWowPackages.stable
-    winetricks
-
-    ### dev & scripting
-    stdenv.cc # aka build-essential
-    autoconf
-    automake
-    libtool
-    pkg-config
-    # shtool - not found
-    # git - included in system packages
-    jq
-
-    ### python
-    (python3.withPackages (ps: with ps; [
-          pip
-          pwntools
-          scapy
-          requests
-          paramiko
-          cryptography
-          pycryptodome
-          jupyter-lab
-        ]))
-
-    ### cryptography
-    openssl
-    gnupg
-
-    ### ops & visibility
-    tmux
-    asciinema
-    flameshot
-    polybar
-    # htop - included in system packages
-  ];
 
   programs.bash.enable = true;
   programs.git = {
---
+++ home-manager/user-packages.nix
@@ -0,0 +1,73 @@
+{ ... }:
+
+{
+home.packages = with pkgs; [
+    ### network analysis
+    # wireshark - in system packages: requires low-level system access
+    # tcpdump - same as wireshark
+    sniffnet
+    aircrack-ng
+    ethtool
+    util-linux
+    net-tools
+
+    ### web security
+    burpsuite
+    zap # aka zaproxy
+    nikto
+    sqlmap
+    dirb
+    # curl - already exists by default
+    wget
+
+    ### vulnerability assessment
+    openvas-scanner
+    thc-hydra
+    john
+    hashcat
+
+    ### reverse engineering
+    binwalk
+    binutils
+    strace
+    ltrace
+    steghide
+    wineWowPackages.stable
+    winetricks
+
+    ### dev & scripting
+    stdenv.cc # aka build-essential
+    autoconf
+    automake
+    libtool
+    pkg-config
+    # shtool - not found
+    # git - included in system packages
+    jq
+    jupyter-lab
+
+    ### python
+    (python3.withPackages (ps: with ps; [
+          pip
+          pwntools
+          scapy
+          requests
+          paramiko
+          cryptography
+          pycryptodome
+          jupyterlab
+        ]))
+
+    ### cryptography
+    openssl
+    gnupg
+
+    ### ops & visibility
+    tmux
+    asciinema
+    flameshot
+    polybar
+    # htop - included in system packages
+  ];
+}
+
